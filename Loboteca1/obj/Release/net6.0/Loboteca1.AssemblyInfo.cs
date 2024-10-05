324e7702d1a672507f436adb1527aa862e450329
JKL-2106 <josuepauliny02022017@gmail.com>
2024-10-05 17:05:55 -0600

    Pruyeba

--- Loboteca1/obj/Release/net6.0/Loboteca1.AssemblyInfo.cs
+++ Loboteca1/obj/Release/net6.0/Loboteca1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4307737840a9396605576a299143e01e7c815009")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
