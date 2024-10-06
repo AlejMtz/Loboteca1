31bcf1ae084e78f7ab97694a22321beafcc513d2
AlejMtz <145378489+AlejMtz@users.noreply.github.com>
2024-10-05 23:49:48 -0600

    Menu desplegable en seccion de sanciones

--- Loboteca1/wwwroot/js/submenu.js
+++ Loboteca1/wwwroot/js/submenu.js
@@ -1,1 +1,11 @@
-﻿
+﻿// Función para mostrar/ocultar el submenú
+function toggleSubMenu() {
+    var submenu = document.getElementById("submenuCarrera");
+
+    // Alterna la clase 'show' para mostrar/ocultar el submenú
+    if (submenu.classList.contains("show")) {
+        submenu.classList.remove("show");  // Si tiene la clase, la quita (oculta el menú)
+    } else {
+        submenu.classList.add("show");  // Si no tiene la clase, la añade (muestra el menú)
+    }
+}
