a2fe29c64ef67517d58d5c1b02d6d73f9bbe2e4f
JKL-2106 <josuepauliny02022017@gmail.com>
2024-10-06 00:05:35 -0600

    Apartado_Tecnologias

---
+++ Loboteca1/Controllers/TecnologiasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Loboteca1.Controllers
+{
+    public class TecnologiasController : Controller
+    {
+        public IActionResult Tecnologias()
+        {
+            return View();
+        }
+    }
+}
