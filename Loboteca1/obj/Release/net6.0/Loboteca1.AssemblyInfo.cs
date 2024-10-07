abe3e1e231557469d410b564ae914d9ba8663f59
JKL-2106 <josuepauliny02022017@gmail.com>
2024-10-06 19:39:31 -0600

    Correcón de iconos

--- Loboteca1/obj/Release/net6.0/Loboteca1.AssemblyInfo.cs
+++ Loboteca1/obj/Release/net6.0/Loboteca1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51ff824a1b38cd10d45af6cb1509e0ce9996823")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
