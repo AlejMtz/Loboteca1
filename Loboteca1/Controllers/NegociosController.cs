3a8f3657b661aa76ff4de3c676eca37c39b1d735
JKL-2106 <josuepauliny02022017@gmail.com>
2024-10-08 21:36:21 -0600

    vistas

---
+++ Loboteca1/Controllers/NegociosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Loboteca1.Controllers
+{
+    public class NegociosController : Controller
+    {
+        public IActionResult Negocios()
+        {
+            return View();
+        }
+    }
+}
--- Loboteca1/obj/Release/net6.0/Loboteca1.AssemblyInfo.cs
+++ Loboteca1/obj/Release/net6.0/Loboteca1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe3e1e231557469d410b564ae914d9ba8663f59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22febee90c162992e18ac75244f4f1eacb2e73f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loboteca1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
